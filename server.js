dbdd293caa5824dec9786352092a7dd738da8123
Glitch (timcoder-fcc-filemetadata) <none>
2018-12-06 09:33:15 +0000

    🐮💗 Checkpoint ./package.json:2374645/989 ./server.js:2374645/258

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
-
+const multer = require('multer');
 var app = express();
 
 app.use(cors());
@@ -16,6 +16,11 @@ app.get('/', function (req, res) {
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
+});
+
+app.post('/api/fileanalyse', multer({dest: 'uploads/'}).single('tempFile'), (req, res) => {
+  res.json({filename: req.file.originalname, file_size: req.file.size});
+  console.log(req.file);
 });
 
 app.listen(process.env.PORT || 3000, function () {
