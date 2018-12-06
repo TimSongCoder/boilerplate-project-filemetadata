b2eb80f37a0ddec8485bf09066315119b02155df
Glitch (timcoder-fcc-filemetadata) <none>
2018-12-06 11:39:19 +0000

    ⛄️📚 Checkpoint ./server.js:2374645/197

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var cors = require('cors');
+const fs = require('fs');
 
 // require and use "multer"...
 const multer = require('multer');
@@ -21,6 +22,12 @@ app.get('/hello', function(req, res){
 app.post('/api/fileanalyse', multer({dest: process.cwd() + '/uploads/'}).single('upfile'), (req, res) => {
   res.json({filename: req.file.originalname, file_size: req.file.size});
   console.log(req.file);
+  // Delete the uploaded file after response sending
+  fs.unlink(req.file.path, err => {
+    if(err){
+      throw err;
+    }
+  });
 });
 
 app.listen(process.env.PORT || 3000, function () {
